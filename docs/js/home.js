bc2ad39c03e0e309a6bbc3580debdf46c8b1ddde
Sylvain UTARD <sylvain.utard@gmail.com>
2015-12-18 14:29:41 +0100

    chore(website): simple join the program form

---
+++ docs/js/home.js
@@ -0,0 +1,23 @@
+(function($) {
+  $('#join-form').on('submit', function() {
+    var $button = $(this).find('button');
+    var $email = $(this).find('input[name="email"]');
+    var $url = $(this).find('input[name="url"]');
+    $button.attr('disabled', true);
+    $.ajax({
+      url: 'https://www.algolia.com/docsearch/join',
+      type: 'POST',
+      data: {
+        email: $email.val(),
+        url: $url.val()
+      }
+    }).done(function() {
+      $email.val('');
+      $url.val('');
+      $button.text('Thank you!');
+    }).fail(function() {
+      $button.attr('disabled', null);
+    })
+    return false;
+  });
+})(jQuery);
