e59d132c2da5eda8971bade607460aa71dbd1d34
Sylvain UTARD <sylvain.utard@gmail.com>
2015-12-28 14:52:33 +0100

    feat(website): ensure the inputs are mandatory
    Move the forms to a partial (had a hard time with HAML-based partials...)
    Small box-shadow if not specified
    Add a "I'm the owner of that website" checkbox

--- docs/_plugins/haml.rb
+++ docs/_plugins/haml.rb
@@ -1,28 +1,40 @@
+require 'jekyll-haml'
+
 module Jekyll
-  class HamlConverter < Converter
-    safe true
 
-    def setup
-      return if @setup
-      require 'haml'
-      @setup = true
-    rescue
-      STDERR.puts 'do `gem install haml`'
-      raise FatalException.new("Missing dependency: haml")
+  class HamlPartialTag < Liquid::Tag
+    def initialize(tag_name, file, tokens)
+      super
+      @file = file.strip
     end
 
-    def matches(ext)
-      ext =~ /haml/i
-    end
+    def render(context)
+      includes_dir = File.join(context.registers[:site].source, '_includes')
 
-    def output_ext(ext)
-      ".html"
-    end
+      if File.symlink?(includes_dir)
+        return "Includes directory '#{includes_dir}' cannot be a symlink"
+      end
 
-    def convert(content)
-      setup
-      engine = Haml::Engine.new(content)
-      engine.render
+      if @file !~ /^[a-zA-Z0-9_\/\.-]+$/ || @file =~ /\.\// || @file =~ /\/\./
+        return "Include file '#{@file}' contains invalid characters or sequences"
+      end
+
+      return "File must have \".haml\" extension" if @file !~ /\.haml$/
+
+      Dir.chdir(includes_dir) do
+        choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+        if choices.include?(@file)
+          source     = File.read(@file)
+          conversion = ::Haml::Engine.new(source).render
+          partial    = Liquid::Template.parse(conversion)
+          return partial.render!(context).gsub(/\s+/, ' ')
+        else
+          "Included file '#{@file}' not found in _includes directory"
+        end
+      end
     end
   end
+
 end
+
+Liquid::Template.register_tag('haml', Jekyll::HamlPartialTag)
--- docs/js/home.js
+++ docs/js/home.js
@@ -3,6 +3,24 @@
     var $button = $(this).find('button');
     var $email = $(this).find('input[name="email"]');
     var $url = $(this).find('input[name="url"]');
+    var $owner = $(this).find('input[name="owner"]');
+    var error = false;
+    $(this).find('.has-errors').removeClass('has-errors');
+    if (!$email.val()) {
+      $email.closest('.input-group').addClass('has-errors');
+      error = true;
+    }
+    if (!$url.val()) {
+      $url.closest('.input-group').addClass('has-errors');
+      error = true;
+    }
+    if (!$owner.is(':checked')) {
+      $owner.addClass('has-errors');
+      error = true;
+    }
+    if (error) {
+      return false;
+    }
     $button.attr('disabled', true);
     $.ajax({
       url: 'https://www.algolia.com/docsearch/join',
