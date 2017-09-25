ae615e73739c9d3ff033f749e9cde2b0f11d071b
Lucas Bonomi <lucas.bonomi@gmail.com>
2017-09-25 14:02:26 +0200

    Start new design

--- docs/js/home.js
+++ docs/js/home.js
@@ -1,51 +1,23 @@
-(function($) {
-  function pardotAppendIframe(url) {
-    var iframe = document.createElement('iframe');
-    iframe.src = url;
-    iframe.width = 1;
-    iframe.height = 1;
-    document.body.appendChild(iframe);
-  }
+function setScreen() {
+  const screens = [
+    'screen_react',
+    'screen_laravel',
+    'screen_akka',
+    'screen_bootstrap',
+    'screen_vue',
+    'screen_middleman',
+  ];
+  const rand = Math.floor(Math.random() * (screens.length - 0) + 0);
+  const imgPath = './img/screens/';
+  const screenImage = document.createElement('img');
+  const imageHolder = document.querySelector('#demo-screens');
+  const imagePlaceholder = document.querySelector('#placeholder');
 
-  $('.join-form').on('submit', function(e) {
-    var $button = $(this).find('button');
-    var $email = $(this).find('input[name="email"]');
-    var $url = $(this).find('input[name="url"]');
-    var $owner = $(this).find('input[name="owner"]');
-    var error = false;
-    $(this).find('.has-errors').removeClass('has-errors');
-    if (!$email.val()) {
-      $email.closest('.input-group').addClass('has-errors');
-      error = true;
-    }
-    if (!$url.val()) {
-      $url.closest('.input-group').addClass('has-errors');
-      error = true;
-    }
-    if (!$owner.is(':checked')) {
-      $owner.addClass('has-errors');
-      error = true;
-    }
-    if (error) {
-      return false;
-    }
-    $button.attr('disabled', true);
-    $.ajax({
-      url: 'https://www.algolia.com/docsearch/join',
-      type: 'POST',
-      data: {
-        email: $email.val(),
-        url: $url.val()
-      }
-    }).done(function() {
-      pardotAppendIframe('https://go.pardot.com/l/139121/2016-08-05/ldp67?email=' + encodeURIComponent($email.val()) + '&website=' + encodeURIComponent($url.val()));
-      $('.join-form').hide();
-      $('.join-form-validated').show();
-    }).fail(function() {
-      $button.attr('disabled', null);
-      alert('An error occurred, please try again later.');
-    });
+  screenImage.classList.add('demo-screen', 'w100p');
+  screenImage.src = imgPath + screens[rand] + '.png';
 
-    return false;
-  });
-})(jQuery);
+  imagePlaceholder.remove();
+  imageHolder.appendChild(screenImage);
+}
+
+window.addEventListener('load', setScreen());
