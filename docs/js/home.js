9663e0154364df2aced028835b61a0305baa7b0b
Lucas Bonomi <lucas.bonomi@gmail.com>
2017-10-17 22:16:04 +0200

    docs(website): new design and logo

--- docs/js/home.js
+++ docs/js/home.js
@@ -35,17 +35,51 @@
       type: 'POST',
       data: {
         email: $email.val(),
-        url: $url.val()
-      }
-    }).done(function() {
-      pardotAppendIframe('https://go.pardot.com/l/139121/2016-08-05/ldp67?email=' + encodeURIComponent($email.val()) + '&website=' + encodeURIComponent($url.val()));
-      $('.join-form').hide();
-      $('.join-form-validated').show();
-    }).fail(function() {
-      $button.attr('disabled', null);
-      alert('An error occurred, please try again later.');
-    });
+        url: $url.val(),
+      },
+    })
+      .done(function() {
+        pardotAppendIframe(
+          'https://go.pardot.com/l/139121/2016-08-05/ldp67?email=' +
+            encodeURIComponent($email.val()) +
+            '&website=' +
+            encodeURIComponent($url.val())
+        );
+        $('.join-form').hide();
+        $('.join-form-validated').show();
+      })
+      .fail(function() {
+        $button.attr('disabled', null);
+        alert('An error occurred, please try again later.');
+      });
 
     return false;
   });
 })(jQuery);
+
+function setScreen() {
+  const screens = [
+    'screen_react',
+    'screen_laravel',
+    'screen_akka',
+    'screen_bootstrap',
+    'screen_vue',
+    'screen_middleman',
+  ];
+  const rand = Math.floor(Math.random() * (screens.length - 0) + 0);
+  const imgPath = './img/screens/';
+  const screenImage = document.createElement('img');
+  const imageHolder = document.querySelector('#demo-screens');
+  const imagePlaceholder = document.querySelector('#placeholder');
+
+  screenImage.classList.add('demo-screen', 'w100p', 'elevation1');
+  screenImage.src = imgPath + screens[rand] + '.png';
+
+  imagePlaceholder.remove();
+  imageHolder.appendChild(screenImage);
+}
+
+window.addEventListener('load', function() {
+  setScreen();
+  document.body.classList.add('ready');
+});
