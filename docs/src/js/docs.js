831a3397f5cdd74042c4fea95dd9216a4e62c392
Shipow <shipowlata@gmail.com>
2018-08-14 19:24:24 +0200

    fixes

--- docs/src/js/docs.js
+++ docs/src/js/docs.js
@@ -2,7 +2,9 @@ function toggleNav() {
   const sidebar = document.getElementsByClassName('navigation-sidebar')[0];
   const open = document.getElementsByClassName('navigation-open')[0];
   const close = document.getElementsByClassName('navigation-close')[0];
+  const body = document.getElementsByTagName('body')[0];
   sidebar.classList.toggle('hidden');
   open.classList.toggle('hidden');
   close.classList.toggle('hidden');
+  body.classList.toggle('navigation-menu-open');
 }
