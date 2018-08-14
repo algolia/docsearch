ea401910b91891b6af15293d584878cfd0624e56
Shipow <shipowlata@gmail.com>
2018-08-14 15:22:31 +0200

    responsive docs+nav / scripts

---
+++ docs/src/js/docs.js
@@ -0,0 +1,4 @@
+function toggleNav() {
+  console.log(this);
+  // element.classList.add("mystyle");
+}
--- docs/src/js/main.js
+++ docs/src/js/main.js
@@ -1,71 +1,8 @@
-/* global Glide, docsearch, Popper */
+/* global docsearch */
 
-window.onload = function() {
-  let i = 0;
-  let txt;
-  let query = '';
-
-  docsearch({
-    apiKey: 'e3082526e1d5f37055241c335ab10f72',
-    indexName: 'talksearch',
-    inputSelector: '.custom-search-input',
-    debug: false,
-  });
-
-  const demoDocsearch = docsearch({
-    apiKey: '5990ad008512000bba2cf951ccf0332f',
-    indexName: 'bootstrap',
-    inputSelector: '.docsearch-live-demo-input',
-    enhancedSearchInput: true,
-    debug: false,
-  });
-
-  const reference = document.querySelector(
-    '.docsearch-live-demo-input-wrapper'
-  );
-  const popper = document.querySelector('.my-popper');
-
-  const anotherPopper = new Popper(reference, popper, {
-    placement: 'bottom-start',
-  });
-
-  function openDocsearch(sampleQuery) {
-    demoDocsearch.autocomplete.autocomplete.setVal(sampleQuery);
-    demoDocsearch.autocomplete.autocomplete.open();
-  }
-
-  function typeWriter() {
-    const speed = 250;
-    if (i < txt.length) {
-      query += txt.charAt(i);
-      openDocsearch(query);
-      i++;
-      setTimeout(typeWriter, speed);
-    }
-  }
-
-  document.querySelectorAll('.ds-sample-query').forEach(el => {
-    el.onclick = function() {
-      txt = el.getAttribute('data-sample');
-      typeWriter();
-    };
-  });
-
-  demoDocsearch.autocomplete.on('autocomplete:opened', () => {
-    anotherPopper.destroy();
-  });
-
-  new Glide('.glide', {
-    type: 'carousel',
-    startAt: 0,
-    perView: 16,
-    autoplay: 1300,
-    breakpoints: {
-      400: { perView: 4 },
-      600: { perView: 6 },
-      800: { perView: 8 },
-      1000: { perView: 10 },
-      1200: { perView: 12 },
-    },
-  }).mount();
-};
+docsearch({
+  apiKey: 'e3082526e1d5f37055241c335ab10f72',
+  indexName: 'talksearch',
+  inputSelector: '.custom-search-input',
+  debug: false,
+});
