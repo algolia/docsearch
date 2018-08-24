7a3102b4387c5a14bcfb97118db2e9594f002f3a
Pixelastic <tim@pixelastic.com>
2018-08-24 13:23:46 +0200

    Adding Prettier for markdown prettification

---
+++ docs/.prettierrc.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-commonjs */
+module.exports = {
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        printWidth: 80,
+        parser: 'markdown',
+      },
+    },
+  ],
+};
