70dd630c338d36552db70b7430f5b0e9887e9c42
François Chalifour <francois.chalifour@gmail.com>
2019-07-11 16:27:11 +0200

    fix(renderer): display loading indicator as SVG

--- packages/docsearch-renderer-downshift/src/Autocomplete.tsx
+++ packages/docsearch-renderer-downshift/src/Autocomplete.tsx
@@ -137,6 +137,31 @@ export class Autocomplete extends Component<
                 </svg>
               </button>
 
+              <div className="algolia-docsearch-loadingIndicator">
+                <svg
+                  viewBox="0 0 38 38"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke="currentColor"
+                  stroke-opacity=".5"
+                >
+                  <g fill="none" fill-rule="evenodd">
+                    <g transform="translate(1 1)" stroke-width="2">
+                      <circle stroke-opacity=".3" cx="18" cy="18" r="18" />
+                      <path d="M36 18c0-9.94-8.06-18-18-18">
+                        <animateTransform
+                          attributeName="transform"
+                          type="rotate"
+                          from="0 18 18"
+                          to="360 18 18"
+                          dur="1s"
+                          repeatCount="indefinite"
+                        />
+                      </path>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+
               <input
                 {...getInputProps({
                   placeholder: this.props.placeholder,
