a284abb6f0934eb2ae313407511541106a0a5baf
François Chalifour <francois.chalifour@gmail.com>
2019-07-14 13:21:30 +0200

    chore(examples): allow configuration of DocSearch indexes

--- examples/vanilla/app.js
+++ examples/vanilla/app.js
@@ -2,8 +2,17 @@ import docsearch from 'docsearch.js';
 import 'docsearch-theme-light';
 import './docsearch-theme.css';
 
+const searchParams = new URL(location).searchParams;
+const indexName = searchParams.get('indexName') || 'francoischalifour';
+const apiKey = searchParams.get('apiKey') || '05f2b9f825e93891445000c63e103290';
+const indexNameInput = document.querySelector('#indexName');
+const apiKeyInput = document.querySelector('#apiKey');
+
+indexNameInput.value = indexName;
+apiKeyInput.value = apiKey;
+
 docsearch({
-  apiKey: '05f2b9f825e93891445000c63e103290',
-  indexName: 'francoischalifour',
+  indexName,
+  apiKey,
   container: '#searchbox',
 });
