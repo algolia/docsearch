c3bdb12e0db92169d87629b66636c6b9ed0ddcd3
Eunjae Lee <eunjae.lee@algolia.com>
2019-08-21 11:10:00 +0200

    chore: add release script (#786)
    * chore: add shipjs
    
    * chore: update versions to 3.0.0-beta.0
    
    * revert: chore: update versions to 3.0.0-beta.0
    
    This reverts commit 4b47fcf915182df4e8b4728f23b8b068cea2197f.
    
    * chore: update shipjs to 0.4.0
    
    * chore: try to release every commit

---
+++ ship.config.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line import/no-commonjs
+module.exports = {
+  mergeStrategy: {
+    toSameBranch: ['next'],
+  },
+  packageJsons: [
+    'lerna.json',
+    'packages/docsearch/package.json',
+    'packages/docsearch-core/package.json',
+    'packages/docsearch-renderer-downshift/package.json',
+    'packages/docsearch-theme-light/package.json',
+    'packages/docsearch-types/package.json',
+  ],
+  versionUpdated: ({ version, exec }) => {
+    [
+      `npx json -I -f examples/vanilla/package.json -e 'this.dependencies["docsearch-theme-light"] = "${version}"'`,
+      `npx json -I -f examples/vanilla/package.json -e 'this.dependencies["docsearch.js"] = "${version}"'`,
+      `npx json -I -f packages/docsearch-core/package.json -e 'this.dependencies["docsearch-types"] = "${version}"'`,
+      `npx json -I -f packages/docsearch-renderer-downshift/package.json -e 'this.dependencies["docsearch-core"] = "${version}"'`,
+      `npx json -I -f packages/docsearch-renderer-downshift/package.json -e 'this.dependencies["docsearch-types"] = "${version}"'`,
+      `npx json -I -f packages/docsearch/package.json -e 'this.dependencies["docsearch-core"] = "${version}"'`,
+      `npx json -I -f packages/docsearch/package.json -e 'this.dependencies["docsearch-renderer-downshift"] = "${version}"'`,
+      `npx json -I -f packages/docsearch/package.json -e 'this.dependencies["docsearch-types"] = "${version}"'`,
+      `echo "export default '${version}';" > packages/docsearch-core/src/version.ts`,
+    ].forEach(exec);
+  },
+  publishCommand: () =>
+    `lerna exec --scope docsearch* -- yarn publish --no-git-tag-version --non-interactive`,
+};
