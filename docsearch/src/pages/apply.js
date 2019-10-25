ed36122863c1e1b7355b79e458734efa5b54f734
s-pace <sylvain.pace@algolia.com>
2019-10-25 14:21:15 +0200

    feat: turn apply.js into a raw js function to enable React hook useBaseUrl

--- docsearch/src/pages/apply.js
+++ docsearch/src/pages/apply.js
@@ -2,27 +2,26 @@ import React from "react";
 import Layout from "@theme/Layout";
 import { Section, Hero } from "@algolia/ui-library";
 import ApplyForm from "./applyComponent.js";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
-class Apply extends React.Component {
-  render() {
-    return (
-      <Layout
-        title="DocSearch: Search made for documentation"
-        description="The easiest way to add search to your documentation - Powered by Algolia"
-      >
-        <Hero
-          style={{ backgroundImage: "linear-gradient(#fff, #f5f5fa)" }}
-          background="curves"
-          title={<img src="img/docsearch-logo.svg" alt="DocSearch" />}
-        />
-        <Section background="white">
-          <ApplyForm />
-        </Section>
-      </Layout>
-    );
-  }
+function Apply() {
+  return (
+    <Layout
+      title="DocSearch: Search made for documentation"
+      description="The easiest way to add search to your documentation - Powered by Algolia"
+    >
+      <Hero
+        style={{ backgroundImage: "linear-gradient(#fff, #f5f5fa)" }}
+        background="curves"
+        title={
+          <img src={useBaseUrl("img/docsearch-logo.svg")} alt="DocSearch" />
+        }
+      />
+      <Section background="white">
+        <ApplyForm />
+      </Section>
+    </Layout>
+  );
 }
-
-Apply.title = "Apply";
 
 export default Apply;
