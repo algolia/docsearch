22a0c3ceb28b5e8e2d0aa14374bb7300a7ebef4c
s-pace <sylvain.pace@algolia.com>
2019-10-28 18:10:32 +0100

    debug: avoid exmplictly custom loader to match mds

--- docsearch/plugins/my-loaders/index.js
+++ docsearch/plugins/my-loaders/index.js
@@ -1,16 +1,20 @@
-module.exports = function(context, options) {
+module.exports = function (context, options) {
+  console.log("my loader")
+  console.log(process.env.NODE_ENV)
   return {
-    name: 'loaders',
+    name: "loaders",
     configureWebpack(config, isServer) {
       return {
         module: {
           rules: [
-            { test: /\.(gif|png|jpe?g|svg)$/i,
-             use: ['file-loader',{ loader: 'image-webpack-loader' }]
+            {
+              test: /\.(gif|png|jpe?g|svg)$/i,
+              exclude: /[\/]docs[\/]/i,
+              use: ["file-loader", { loader: "image-webpack-loader" }]
             }
           ]
         }
-      }
+      };
     }
-  }
+  };
 };
