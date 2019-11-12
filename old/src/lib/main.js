bc04b49a31638809a24ee65b1ed912300f5a82c7
s-pace <sylvain.pace@algolia.com>
2019-11-12 18:03:22 +0100

    refactor: remove legacy code

--- old/src/lib/main.js
+++
@@ -1,8 +0,0 @@
-import toFactory from 'to-factory';
-import DocSearch from './DocSearch';
-import version from './version';
-
-const docsearch = toFactory(DocSearch);
-docsearch.version = version;
-
-export default docsearch;
