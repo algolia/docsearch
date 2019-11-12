bf6a23a076e9b746e35b87057e9547601e4852a2
François Chalifour <francoischalifour@users.noreply.github.com>
2019-11-12 10:42:42 +0100

    feat(projects): generate demo and showcase projects grid (#882)
    * feat(projects): generate demo and showcase projects grid
    
    * fix(sjowcase): enhance alt attribute on YOU logo
    
    * fix(showcase&demo): use same names as docusaurus

--- docsearch/src/pages/index.js
+++ docsearch/src/pages/index.js
@@ -23,6 +23,8 @@ import {
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
+import showcaseProjects from "./showcase-projects.json";
+import demoProjects from "./demo-projects.json";
 import ApplyForm from "../components/ApplyForm.js";
 
 function Home() {
@@ -73,440 +75,30 @@ function Home() {
           className="jc-between d-flex m-auto fx-wrap"
           style={{ maxWidth: "800px" }}
         >
-          <div className="ta-center w-20p">
-            <a
-              href="https://getbootstrap.com/docs/4.1/getting-started/introduction/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/bootstrap.jpg")}
-                alt={"Bootstrap logo"}
-              />
-              <SmallText tag="p">Bootstrap</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://babeljs.io/docs/en/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/babel.jpg")}
-                alt={"Babel logo"}
-              />
-              <SmallText tag="p">Babel</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://graphql.org/learn/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/graphql.jpg")}
-                alt={"Graphql logo"}
-              />
-              <SmallText tag="p">Graphql</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://www.typescriptlang.org/index.html"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/typescriptlang.svg")}
-                alt={"TypeScript logo"}
-              />
-              <SmallText tag="p">TypeScript</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://reactjs.org/docs/getting-started.html"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/react.jpg")}
-                alt={"React logo"}
-              />
-              <SmallText tag="p">React</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://requests.readthedocs.io/en/master/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/requests.png")}
-                alt={"Requests logo"}
-              />
-              <SmallText tag="p">Requests</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://eslint.org/" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/eslint.svg")}
-                alt={"ESLint logo"}
-              />
-              <SmallText tag="p">ESLint</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://webpack.js.org/concepts/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/webpack.svg")}
-                alt={"Webpack logo"}
-              />
-              <SmallText tag="p">Webpack</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://www.gatsbyjs.org/docs/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/gatsby.png")}
-                alt={"Gatsby logo"}
-              />
-              <SmallText tag="p">Gatsby</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://docs.netlify.com/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/netlify.svg")}
-                alt={"Netlify logo"}
-              />
-              <SmallText tag="p">Netlify</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://vuejs.org/" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/vue.svg")}
-                alt={"Vue logo"}
-              />
-              <SmallText tag="p">Vue</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://yarnpkg.com/lang/en/docs/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/yarn.jpg")}
-                alt={"Yarn logo"}
-              />
-              <SmallText tag="p">Yarn</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://material-ui.com" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/material-ui.svg")}
-                alt={"MATERIAL-UI logo"}
-              />
-              <SmallText tag="p">MATERIAL-UI</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://ant.design/index-cn"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/ant-design.svg")}
-                alt={"Ant Design logo"}
-              />
-              <SmallText tag="p">Ant Design</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://facebook.github.io/react-native/docs/getting-started"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/react-native.svg")}
-                alt={"React Native logo"}
-              />
-              <SmallText tag="p">React Native</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://api.jquery.com/" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/jquery.jpg")}
-                alt={"jQuery logo"}
-              />
-              <SmallText tag="p">jQuery</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://docs.gitlab.com/" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/gitlab.svg")}
-                alt={"GitLab logo"}
-              />
-              <SmallText tag="p">GitLab</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://brew.sh/" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/homebrew.png")}
-                alt={"Homebrew logo"}
-              />
-              <SmallText tag="p">Homebrew</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://momentjs.com/docs/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/momentjs.svg")}
-                alt={"Moment.js logo"}
-              />
-              <SmallText tag="p">Moment.js</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://tailwindcss.com/docs/installation/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/tailwindcss.svg")}
-                alt={"Tailwindcss logo"}
-              />
-              <SmallText tag="p">tailwindcss</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://bootstrap-vue.js.org/docs"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/bootstrap-vue.svg")}
-                alt={"BootstrapVue logo"}
-              />
-              <SmallText tag="p">BootstrapVue</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://sass-lang.com/" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/sass.png")}
-                alt={"Sass logo"}
-              />
-              <SmallText tag="p">Sass</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://www.twilio.com/docs"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/twilio.png")}
-                alt={"Twilio logo"}
-              />
-              <SmallText tag="p">Twilio</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://docusaurus.io/" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/docusaurus.svg")}
-                alt={"Docusaurus logo"}
-              />
-              <SmallText tag="p">Docusaurus</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://docs.gradle.org/current/userguide/userguide.html"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/gradle.png")}
-                alt={"Gradle logo"}
-              />
-              <SmallText tag="p">Gradle</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://nuxtjs.org/" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/nuxtjs.svg")}
-                alt={"NUXTJS logo"}
-              />
-              <SmallText tag="p">NUXTJS</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://vuetifyjs.com/en/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/vuetify.png")}
-                alt={"Vuetify.js logo"}
-              />
-              <SmallText tag="p">Vuetify.js</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://jekyllrb.com/" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/jekyll.png")}
-                alt={"Jekyll logo"}
-              />
-              <SmallText tag="p">Jekyll</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://pytorch.org/get-started/locally/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/pytorch.svg")}
-                alt={"PyTorch logo"}
-              />
-              <SmallText tag="p">PyTorch</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://www.apollographql.com/docs/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/apollo.png")}
-                alt={"Apollo logo"}
-              />
-              <SmallText tag="p">Apollo</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://docs.scala-lang.org/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/scala.svg")}
-                alt={"Scala logo"}
-              />
-              <SmallText tag="p">Scala</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a
-              href="https://laravel.com/docs/6.x"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/laravel.svg")}
-                alt={"Laravel logo"}
-              />
-              <SmallText tag="p">Laravel</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://socket.io/docs/" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/socketio.svg")}
-                alt={"Socket.io logo"}
-              />
-              <SmallText tag="p">socket.io</SmallText>
-            </a>
-          </div>
-          <div className="ta-center w-20p">
-            <a href="https://jestjs.io/" rel="noreferrer" target="_blank">
-              <img
-                className="h-50"
-                src={useBaseUrl("img/logos/jest.png")}
-                alt={"Jest logo"}
-              />
-              <SmallText tag="p">Jest</SmallText>
-            </a>
-          </div>
+          {showcaseProjects.map(({ name, href, image }) => (
+            <div key={href} className="ta-center w-20p">
+              <a
+                href={href}
+                rel="noreferrer"
+                target="_blank"
+                alt={`Discover DocSearch on the ${name} documentation`}
+              >
+                <img
+                  className="h-50"
+                  src={useBaseUrl(image)}
+                  alt={`Discover DocSearch on the ${name} documentation`}
+                />
+                <SmallText tag="p">{name}</SmallText>
+              </a>
+            </div>
+          ))}
+
           <div className="ta-center w-20p">
             <a href={useBaseUrl("/apply")} rel="noreferrer" target="_blank">
               <img
                 className="h-50"
                 src={useBaseUrl("img/logos/three-dots.svg")}
-                alt={"Your project logo?"}
+                alt="Your project logo? Apply for DocSearch"
               />
               <SmallText tag="p">You?</SmallText>
             </a>
@@ -605,88 +197,22 @@ function Home() {
           </Text>
         </SectionHeader>
         <CardsRow>
-          <Card
-            image="img/demos/example-bootstrap.gif"
-            imageAlt="Bootstrap demo"
-          >
-            <LightCta
-              withArrow
-              href="https://getbootstrap.com/docs/4.3/getting-started/introduction/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                style={{
-                  marginTop: "-2px",
-                  marginRight: "12px"
-                }}
-                className={"h-30 va-middle"}
-                src={useBaseUrl("img/logos/bootstrap.jpg")}
-                alt={"Bootstrap logo"}
-              />
-              Visit Bootstrap
-            </LightCta>
-          </Card>
-          <Card image="img/demos/example-vuejs.gif" imageAlt="Vue demo">
-            <img
-              style={{
-                marginTop: "-2px",
-                marginRight: "12px"
-              }}
-              className={"h-30 va-middle"}
-              src={useBaseUrl("img/logos/vue.svg")}
-              alt={"Vue logo"}
-            />
-            <LightCta
-              withArrow
-              href="https://vuejs.org/v2/guide/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Visit Vue.js
-            </LightCta>
-          </Card>
-          <Card image="img/demos/example-react.gif" imageAlt="React demo">
-            <img
-              style={{
-                marginTop: "-2px",
-                marginRight: "12px"
-              }}
-              className={"h-30 va-middle"}
-              src={useBaseUrl("img/logos/react.jpg")}
-              alt={"React logo"}
-            />
-            <LightCta
-              withArrow
-              href="https://reactjs.org/docs/getting-started.html"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Visit React
-            </LightCta>
-          </Card>
-          <Card
-            image="img/demos/example-momentjs.gif"
-            imageAlt="Moment.js demo"
-          >
-            <img
-              style={{
-                marginTop: "-2px",
-                marginRight: "12px"
-              }}
-              className={"h-30 va-middle"}
-              src={useBaseUrl("img/logos/momentjs.svg")}
-              alt={"Moment.js logo"}
-            />
-            <LightCta
-              withArrow
-              href="https://momentjs.com/docs/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Visit Moment.js
-            </LightCta>
-          </Card>
+          {demoProjects.map(({ name, href, logo, preview }) => (
+            <Card image={preview} imageAlt={`${name} demo`}>
+              <LightCta withArrow href={href} rel="noreferrer" target="_blank">
+                <img
+                  style={{
+                    marginTop: "-2px",
+                    marginRight: "12px"
+                  }}
+                  className={"h-30 va-middle"}
+                  src={useBaseUrl(logo)}
+                  alt={name}
+                />
+                Visit {name}
+              </LightCta>
+            </Card>
+          ))}
         </CardsRow>
       </Section>
 
