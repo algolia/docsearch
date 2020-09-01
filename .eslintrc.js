ad44f0edc4167cd55316c0275d52167451211240
François Chalifour <francois.chalifour@gmail.com>
2020-09-01 14:24:58 +0200

    chore: update tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: ['algolia', 'algolia/jest', 'algolia/react', 'algolia/typescript'],
   globals: {
     __DEV__: false,
-    __VERSION__: false,
   },
   settings: {
     react: {
@@ -25,9 +24,6 @@ module.exports = {
     'react/no-unescaped-entities': 0,
     'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
     'import/extensions': 0,
-    '@typescript-eslint/camelcase': ['error', { allow: ['__autocomplete_id'] }],
-    // Useful to call functions like `nodeItem?.scrollIntoView()`.
-    'no-unused-expressions': 0,
     complexity: 0,
     'import/order': [
       'error',
