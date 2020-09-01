0280cbcd43c4406f80ad3214aa1193fd5540731a
François Chalifour <francois.chalifour@gmail.com>
2020-09-01 11:52:19 +0200

    chore: remove root files

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-commonjs */
-module.exports = {
-  bail: true,
-  resetMocks: true,
-  restoreMocks: true,
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/docs/'],
-};
