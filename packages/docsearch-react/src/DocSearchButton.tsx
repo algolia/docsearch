3a040314aac65071b8d4ead7684cf918714b0311
Sébastien Lorber <slorber@users.noreply.github.com>
2021-02-04 17:26:49 +0100

    Update DocSearchButton.tsx

--- packages/docsearch-react/src/DocSearchButton.tsx
+++ packages/docsearch-react/src/DocSearchButton.tsx
@@ -3,7 +3,12 @@ import React, { useEffect, useState } from 'react';
 import { ControlKeyIcon } from './icons/ControlKeyIcon';
 import { SearchIcon } from './icons/SearchIcon';
 
-export type DocSearchButtonProps = React.ComponentProps<'button'> & {placeholderLabel?: string}
+type Translations = Partial<{
+  buttonText: string;
+  buttonAriaLabel: string;
+}>
+
+export type DocSearchButtonProps = React.ComponentProps<'button'> & {translations?: Translations}
 
 const ACTION_KEY_DEFAULT = 'Ctrl' as const;
 const ACTION_KEY_APPLE = '⌘' as const;
@@ -15,7 +20,12 @@ function isAppleDevice() {
 export const DocSearchButton = React.forwardRef<
   HTMLButtonElement,
   DocSearchButtonProps
->(({placeholderLabel = "Search", ...props}, ref) => {
+>(({translations = {}, ...props}, ref) => {
+  const {
+    buttonText = "Search",
+    buttonAriaLabel = "Search"
+  } = translations;
+  
   const [key, setKey] = useState<
     typeof ACTION_KEY_APPLE | typeof ACTION_KEY_DEFAULT | null
   >(null);
@@ -30,13 +40,13 @@ export const DocSearchButton = React.forwardRef<
     <button
       type="button"
       className="DocSearch DocSearch-Button"
-      aria-label="Search"
+      aria-label={buttonAriaLabel}
       {...props}
       ref={ref}
     >
       <span className="DocSearch-Button-Container">
         <SearchIcon />
-        <span className="DocSearch-Button-Placeholder">{placeholderLabel}</span>
+        <span className="DocSearch-Button-Placeholder">{buttonText}</span>
       </span>
 
       {key !== null ? (
