d2d87cee16323e7c80ddc9a15f991dcc34dc3675
chenxsan <chenxsan@gmail.com>
2021-07-21 20:31:07 +0800

    remove setKey

--- packages/docsearch-react/src/DocSearchButton.tsx
+++ packages/docsearch-react/src/DocSearchButton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { ControlKeyIcon } from './icons/ControlKeyIcon';
 import { SearchIcon } from './icons/SearchIcon';
@@ -25,15 +25,14 @@ export const DocSearchButton = React.forwardRef<
 >(({ translations = {}, ...props }, ref) => {
   const { buttonText = 'Search', buttonAriaLabel = 'Search' } = translations;
 
-  const [key, setKey] = useState<
+  const [key] = useState<
     typeof ACTION_KEY_APPLE | typeof ACTION_KEY_DEFAULT | null
-  >(null);
-
-  useEffect(() => {
+  >(() => {
     if (typeof navigator !== 'undefined') {
-      setKey(isAppleDevice() ? ACTION_KEY_APPLE : ACTION_KEY_DEFAULT);
+      return isAppleDevice() ? ACTION_KEY_APPLE : ACTION_KEY_DEFAULT;
     }
-  }, []);
+    return null;
+  });
 
   return (
     <button
