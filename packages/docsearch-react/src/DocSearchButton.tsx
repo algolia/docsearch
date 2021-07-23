751d400b6814723291f5e7ecb263479359527b00
Clément Vannicatte <vannicattec@gmail.com>
2021-07-23 16:08:07 +0200

    fix: hide `keys` container on mobile

--- packages/docsearch-react/src/DocSearchButton.tsx
+++ packages/docsearch-react/src/DocSearchButton.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import { ControlKeyIcon } from './icons/ControlKeyIcon';
 import { SearchIcon } from './icons/SearchIcon';
@@ -49,12 +49,12 @@ export const DocSearchButton = React.forwardRef<
 
       <span className="DocSearch-Button-Keys">
         {key !== null && (
-          <Fragment>
+          <>
             <span className="DocSearch-Button-Key">
               {key === ACTION_KEY_DEFAULT ? <ControlKeyIcon /> : key}
             </span>
             <span className="DocSearch-Button-Key">K</span>
-          </Fragment>
+          </>
         )}
       </span>
     </button>
