7fad6a46c18f2e08befc6ddf02817bb1d5930828
Clément Vannicatte <20689156+shortcuts@users.noreply.github.com>
2021-07-23 18:36:30 +0200

    fix(button): prevent container keys ui flash (#1033)
    Co-authored-by: François Chalifour <francoischalifour@users.noreply.github.com>

--- packages/docsearch-react/src/DocSearchButton.tsx
+++ packages/docsearch-react/src/DocSearchButton.tsx
@@ -47,14 +47,16 @@ export const DocSearchButton = React.forwardRef<
         <span className="DocSearch-Button-Placeholder">{buttonText}</span>
       </span>
 
-      {key !== null && (
-        <span className="DocSearch-Button-Keys">
-          <span className="DocSearch-Button-Key">
-            {key === ACTION_KEY_DEFAULT ? <ControlKeyIcon /> : key}
-          </span>
-          <span className="DocSearch-Button-Key">K</span>
-        </span>
-      )}
+      <span className="DocSearch-Button-Keys">
+        {key !== null && (
+          <>
+            <span className="DocSearch-Button-Key">
+              {key === ACTION_KEY_DEFAULT ? <ControlKeyIcon /> : key}
+            </span>
+            <span className="DocSearch-Button-Key">K</span>
+          </>
+        )}
+      </span>
     </button>
   );
 });
