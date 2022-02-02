2c1fe7c77c5d5ab28875aa86c9a8ef3b4ffdb254
Clément Vannicatte <20689156+shortcuts@users.noreply.github.com>
2022-02-02 10:39:44 +0100

    feat(noResults): allow providing URL to report missing results (#1289)

--- packages/docsearch-react/src/DocSearchModal.tsx
+++ packages/docsearch-react/src/DocSearchModal.tsx
@@ -50,6 +50,7 @@ export function DocSearchModal({
   disableUserPersonalization = false,
   initialQuery: initialQueryFromProp = '',
   translations = {},
+  getMissingResultsUrl,
 }: DocSearchModalProps) {
   const {
     footer: footerTranslations,
@@ -429,6 +430,7 @@ export function DocSearchModal({
             favoriteSearches={favoriteSearches}
             inputRef={inputRef}
             translations={screenStateTranslations}
+            getMissingResultsUrl={getMissingResultsUrl}
             onItemClick={(item) => {
               saveRecentSearch(item);
               onClose();
