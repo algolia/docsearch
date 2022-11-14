0d35e04614a421ee27ee641ef286e4aa9828faf0
Clément Vannicatte <vannicattec@gmail.com>
2022-11-14 09:56:11 +0100

    docs: reduce Algolia logo size (#1671)

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -162,6 +162,7 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
             alt: 'Algolia',
             src: 'img/Algolia-logo-blue.svg',
             srcDark: 'img/Algolia-logo-white.svg',
+            width: 200,
           },
           copyright: 'DocSearch 2015-now • Designed and built by Algolia',
         },
