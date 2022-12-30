ef95b7e7f8b277495b691412fe0d112e1050d996
Sam Chen <chenxsan@gmail.com>
2022-12-30 19:02:22 +0800

    transform search results

--- examples/js-demo/app.js
+++ examples/js-demo/app.js
@@ -1,4 +1,4 @@
-import docsearch from '@docsearch/js/dist/umd';
+import docsearch from '@docsearch/js/dist/esm';
 
 import './app.css';
 import '@docsearch/css';
@@ -8,4 +8,13 @@ docsearch({
   indexName: 'docsearch',
   appId: 'R2IYF7ETH7',
   apiKey: '599cec31baffa4868cae4e79f180729b',
+  transformItems(items) { // transform absolute url into relative ones to solve CORS problem in cypress
+    return items.map((item) => {
+      const { origin } = new URL(item.url);
+      return {
+        ...item,
+        url: item.url.replace(new RegExp(`^${origin}`), ''),
+      };
+    });
+  },
 });
