d7f5e007047db40437ab8dd7cd387088d889cb4e
Shane Afsar <shane.afsar@algolia.com>
2023-03-30 14:32:49 -0400

    Update Home.js
    update stats

--- packages/website/src/components/Home.js
+++ packages/website/src/components/Home.js
@@ -82,7 +82,7 @@ function Home() {
                           Searches per month
                         </dt>
                         <dd className="order-1 text-5xl leading-none font-extrabold text-indigo-600">
-                          +160M
+                          +170M
                         </dd>
                       </div>
                       <div className="flex flex-col border-t p-6 text-center lg:border-0 lg:border-l showcase-border">
