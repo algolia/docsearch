4c82640022154dd32cc9d3e28a7ae2fba786da8d
Doma <leishenghao@126.com>
2023-10-05 22:57:27 +0800

    feat: add keypress effect to DocSearchButton (#2087)
    * feat: add keypress effect to DocSearchButton
    
    * Bump expected bundle sizes
    
    ---------
    
    Co-authored-by: Paul Jankowski <8BitTitan@gmail.com>
    Co-authored-by: Paul Jankowski <33367713+8bittitan@users.noreply.github.com>

--- packages/docsearch-react/src/DocSearchButton.tsx
+++ packages/docsearch-react/src/DocSearchButton.tsx
@@ -51,13 +51,72 @@ export const DocSearchButton = React.forwardRef<
       <span className="DocSearch-Button-Keys">
         {key !== null && (
           <>
-            <kbd className="DocSearch-Button-Key">
+            <DocSearchButtonKey
+              reactsToKey={
+                key === ACTION_KEY_DEFAULT ? ACTION_KEY_DEFAULT : 'Meta'
+              }
+            >
               {key === ACTION_KEY_DEFAULT ? <ControlKeyIcon /> : key}
-            </kbd>
-            <kbd className="DocSearch-Button-Key">K</kbd>
+            </DocSearchButtonKey>
+            <DocSearchButtonKey reactsToKey="k">K</DocSearchButtonKey>
           </>
         )}
       </span>
     </button>
   );
 });
+
+type DocSearchButtonKeyProps = {
+  reactsToKey?: string;
+};
+
+function DocSearchButtonKey({
+  reactsToKey,
+  children,
+}: React.PropsWithChildren<DocSearchButtonKeyProps>) {
+  const [isKeyDown, setIsKeyDown] = useState(false);
+
+  useEffect(() => {
+    if (!reactsToKey) {
+      return undefined;
+    }
+
+    function handleKeyDown(e: KeyboardEvent) {
+      if (e.key === reactsToKey) {
+        setIsKeyDown(true);
+      }
+    }
+
+    function handleKeyUp(e: KeyboardEvent) {
+      if (
+        e.key === reactsToKey ||
+        // keyup doesn't fire when Command is held down,
+        // workaround is to mark key as also released when Command is released
+        // See https://stackoverflow.com/a/73419500
+        e.key === 'Meta'
+      ) {
+        setIsKeyDown(false);
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [reactsToKey]);
+
+  return (
+    <kbd
+      className={
+        isKeyDown
+          ? 'DocSearch-Button-Key DocSearch-Button-Key--pressed'
+          : 'DocSearch-Button-Key'
+      }
+    >
+      {children}
+    </kbd>
+  );
+}
