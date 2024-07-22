ad3b5af5337e9ae715a0ca9636852a4c5c8bd6ad
James Gray <randombeeper@gmail.com>
2024-07-22 16:24:59 -0600

    linter fixes

--- packages/website/src/components/Home.js
+++ packages/website/src/components/Home.js
@@ -111,27 +111,27 @@ function Home() {
                   return a.name.localeCompare(b.name);
                 })
                 .map(({ name, href, image }) => (
-                <div
-                  key={href}
-                  className="col-span-1 flex justify-center py-2 px-2 text-center"
-                >
-                  <a
-                    href={href}
-                    rel="noreferrer"
-                    target="_blank"
-                    alt={`Discover DocSearch on the ${name} documentation`}
+                  <div
+                    key={href}
+                    className="col-span-1 flex justify-center py-2 px-2 text-center"
                   >
-                    <img
-                      className="inline-block h-10 w-10"
-                      src={withBaseUrl(image)}
+                    <a
+                      href={href}
+                      rel="noreferrer"
+                      target="_blank"
                       alt={`Discover DocSearch on the ${name} documentation`}
-                    />
-                    <div className="text-description uppercase text-xs py-2 font-semibold">
-                      {name}
-                    </div>
-                  </a>
-                </div>
-              ))}
+                    >
+                      <img
+                        className="inline-block h-10 w-10"
+                        src={withBaseUrl(image)}
+                        alt={`Discover DocSearch on the ${name} documentation`}
+                      />
+                      <div className="text-description uppercase text-xs py-2 font-semibold">
+                        {name}
+                      </div>
+                    </a>
+                  </div>
+                ))}
             </div>
           </div>
         </div>
