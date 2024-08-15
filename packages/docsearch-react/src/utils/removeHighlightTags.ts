b5e5d972ddc41b32e5503b2ea459bda393e996c8
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-08-15 20:11:32 +0530

    fix: DocSearch-Hit-source is empty in certain cases

--- packages/docsearch-react/src/utils/removeHighlightTags.ts
+++ packages/docsearch-react/src/utils/removeHighlightTags.ts
@@ -1,24 +1,14 @@
 import type { DocSearchHit, InternalDocSearchHit } from '../types';
 
 const regexHighlightTags = /(<mark>|<\/mark>)/g;
-const regexHasHighlightTags = RegExp(regexHighlightTags.source);
 
 export function removeHighlightTags(
   hit: DocSearchHit | InternalDocSearchHit
 ): string {
-  const internalDocSearchHit = hit as InternalDocSearchHit;
+  const highlightedValue =
+    ('__docsearch_parent' in hit
+      ? hit.__docsearch_parent?._highlightResult?.hierarchy?.lvl0?.value
+      : hit._highlightResult?.hierarchy?.lvl0?.value) ?? hit.hierarchy.lvl0;
 
-  if (!internalDocSearchHit.__docsearch_parent && !hit._highlightResult) {
-    return hit.hierarchy.lvl0;
-  }
-
-  const { value } =
-    (internalDocSearchHit.__docsearch_parent
-      ? internalDocSearchHit.__docsearch_parent?._highlightResult?.hierarchy
-          ?.lvl0
-      : hit._highlightResult?.hierarchy?.lvl0) || {};
-
-  return value && regexHasHighlightTags.test(value)
-    ? value.replace(regexHighlightTags, '')
-    : value;
+  return highlightedValue.replace(regexHighlightTags, '');
 }
