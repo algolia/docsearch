9e292c8a2656b95395efb3221d8528be2b551438
Dylan Tientcheu <dylantientcheu@gmail.com>
2025-04-17 17:44:12 +0200

    fix: add askai demo

---
+++ examples/demo-askai/src/App.tsx
@@ -0,0 +1,22 @@
+import { DocSearch } from '@docsearch/react';
+import type { JSX } from 'react';
+import './App.css';
+import '@docsearch/css/dist/style.css';
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1>DocSearch v4 - AskAI</h1>
+      <DocSearch
+        indexName="beta-react"
+        appId="betaHAXPMHIMMC"
+        apiKey="a7b0d142f53e8b9e6868c4fdbc4374e7"
+        datasourceId="crawler_rag_beta-react-rag"
+        promptId="crawler_rag_beta-react-rag"
+        insights={true}
+      />
+    </div>
+  );
+}
+
+export default App;
