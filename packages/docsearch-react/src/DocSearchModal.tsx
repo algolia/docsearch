4d631864cdf08d4de0a18193a1c9897ef1964b38
Dylan Tientcheu <dylantientcheu@gmail.com>
2025-08-25 15:53:39 +0200

    fix: use proper index

--- packages/docsearch-react/src/DocSearchModal.tsx
+++ packages/docsearch-react/src/DocSearchModal.tsx
@@ -317,7 +317,7 @@ export function DocSearchModal({
   // storage
   const conversations = React.useRef(
     createStoredConversations<StoredAskAiState>({
-      key: `__DOCSEARCH_ASKAI_CONVERSATIONS__${indexName}`,
+      key: `__DOCSEARCH_ASKAI_CONVERSATIONS__${askAiConfig?.indexName}`,
       limit: 10,
     }),
   ).current;
