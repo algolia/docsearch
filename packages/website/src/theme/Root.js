a34113a0439f998fb03a7145b19ff4b2babacace
Paul Jankowski <8BitTitan@gmail.com>
2025-12-19 15:17:10 -0500

    fix: transform and navigate to results based on relative path

--- packages/website/src/theme/Root.js
+++ packages/website/src/theme/Root.js
@@ -2,6 +2,7 @@ import { DocSearch } from '@docsearch/core';
 import { DocSearchButton, DocSearchModal } from '@docsearch/modal';
 import { Sidepanel, SidepanelButton } from '@docsearch/sidepanel';
 import BrowserOnly from '@docusaurus/BrowserOnly';
+import { useHistory } from '@docusaurus/router';
 import React from 'react';
 import { createPortal } from 'react-dom';
 
@@ -12,7 +13,45 @@ const API_KEY = '24b09689d5b4223813d9b8e48563c8f6';
 const ASSISTANT_ID = 'askAIDemo';
 const ASK_AI_INDEX_NAME = 'docsearch-markdown';
 
+function useSearchResultUrlProcessor() {
+  return React.useCallback((url) => {
+    const parsedURL = new URL(url);
+
+    // Otherwise => transform to relative URL for SPA navigation
+    const relativeUrl = `${parsedURL.pathname + parsedURL.hash}`;
+
+    return relativeUrl;
+  }, []);
+}
+
+function useNavigator() {
+  const history = useHistory();
+  const [navigator] = React.useState(() => {
+    return {
+      navigate(params) {
+        history.push(params.itemUrl);
+      },
+    };
+  });
+  return navigator;
+}
+
+function useTransformItems() {
+  const processSearchResultUrl = useSearchResultUrlProcessor();
+  const [transformItems] = React.useState(() => {
+    return (items) =>
+      items.map((item) => ({
+        ...item,
+        url: processSearchResultUrl(item.url),
+      }));
+  });
+  return transformItems;
+}
+
 export default function Root({ children }) {
+  const navigator = useNavigator();
+  const transformItems = useTransformItems();
+
   return (
     <>
       {children}
@@ -39,6 +78,8 @@ export default function Root({ children }) {
                       poweredByText: 'Powered by',
                     },
                   }}
+                  navigator={navigator}
+                  transformItems={transformItems}
                 />
               </>,
               document.querySelector('.navbarSearchContainer_Bca1'),
