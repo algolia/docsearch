c94eafbbee9bb7ae76f11a79795ea10906bcc7ae
Paul Jankowski <8bittitan@gmail.com>
2026-02-20 13:24:32 -0500

    chore: Update to use tsdown for build system (#2824)
    * chore: Update to use tsdown for build system
    
    * fix: docsearch-react build
    
    * fix: lint
    
    * fix: glob resolved to incorrect version

---
+++ tsdown.base.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-unresolved
+import { replacePlugin } from 'rolldown/plugins';
+
+export const rolldownPlugins = [
+  replacePlugin(
+    {
+      __DEV__: JSON.stringify(process.env.NODE_ENV === 'development'),
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    },
+    {
+      preventAssignment: true,
+    },
+  ),
+];
