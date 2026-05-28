346bb0779ad2efe9edb2b05119d2af37aabbf9e3
Felipe Bermudez <felipeberm@gmail.com>
2026-05-28 11:46:06 -0500

    feat(chore): comments and improvements solved

--- packages/website/src/components/CookieSettingsButton.js
+++ packages/website/src/components/CookieSettingsButton.js
@@ -1,6 +1,48 @@
 import React from 'react';
 
+const ONE_TRUST_POLL_INTERVAL_MS = 100;
+const ONE_TRUST_POLL_TIMEOUT_MS = 10_000;
+
+function isOneTrustLoaded() {
+  return typeof window !== 'undefined' && (window.OneTrust !== undefined || window.Optanon !== undefined);
+}
+
+function useOneTrustAvailable() {
+  const [isAvailable, setIsAvailable] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isOneTrustLoaded()) {
+      setIsAvailable(true);
+      return undefined;
+    }
+
+    const intervalId = window.setInterval(() => {
+      if (isOneTrustLoaded()) {
+        setIsAvailable(true);
+        window.clearInterval(intervalId);
+      }
+    }, ONE_TRUST_POLL_INTERVAL_MS);
+
+    const timeoutId = window.setTimeout(() => {
+      window.clearInterval(intervalId);
+    }, ONE_TRUST_POLL_TIMEOUT_MS);
+
+    return () => {
+      window.clearInterval(intervalId);
+      window.clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return isAvailable;
+}
+
 export function CookieSettingsButton() {
+  const isOneTrustAvailable = useOneTrustAvailable();
+
+  if (!isOneTrustAvailable) {
+    return null;
+  }
+
   return (
     <button type="button" id="ot-sdk-btn" className="ot-sdk-show-settings">
       Cookie settings
--- packages/website/src/theme/Footer/Copyright/index.js
+++ packages/website/src/theme/Footer/Copyright/index.js
@@ -2,17 +2,9 @@ import { CookieSettingsButton } from '@site/src/components/CookieSettingsButton'
 import React from 'react';
 
 export default function FooterCopyright({ copyright }) {
-  if (!copyright) {
-    return (
-      <div className="footer__copyright footer__copyright--with-cookie-settings">
-        <CookieSettingsButton />
-      </div>
-    );
-  }
-
   return (
     <div className="footer__copyright footer__copyright--with-cookie-settings">
-      <div dangerouslySetInnerHTML={{ __html: copyright }} />
+      {copyright ? <div dangerouslySetInnerHTML={{ __html: copyright }} /> : null}
       <CookieSettingsButton />
     </div>
   );
