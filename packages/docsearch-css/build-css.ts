2a2a90b8cfe22f4c16060770b00edde8e47e24c0
Paul Jankowski <8bittitan@gmail.com>
2026-07-10 18:16:12 -0400

    chore(v5): Split Ask AI related CSS into own bundle (#2913)
    * chore(v5): Split Ask AI related CSS into own bundle
    
    * move style.css to include modal and askai

--- packages/docsearch-css/build-css.ts
+++ packages/docsearch-css/build-css.ts
@@ -18,15 +18,24 @@ interface Entry {
 const ENTRIES: Entry[] = [
   {
     input: 'src/_variables.css',
-    outputs: ['dist/_variables.css'],
+    outputs: ['dist/_variables.css', 'dist/_variables.scss'],
     bannerName: 'Variables',
   },
   {
     input: 'src/button.css',
-    outputs: ['dist/button.css'],
+    outputs: ['dist/button.css', 'dist/button.scss'],
     bannerName: 'Button',
   },
-  { input: 'src/modal.css', outputs: ['dist/modal.css'], bannerName: 'Modal' },
+  {
+    input: 'src/modal.css',
+    outputs: ['dist/modal.css', 'dist/modal.scss'],
+    bannerName: 'Modal',
+  },
+  {
+    input: 'src/_askai.css',
+    outputs: ['dist/_askai.css', 'dist/_askai.scss'],
+    bannerName: 'Ask AI',
+  },
   { input: 'src/style.css', outputs: ['dist/style.css', 'dist/style.scss'] },
   {
     input: 'src/sidepanel.css',
